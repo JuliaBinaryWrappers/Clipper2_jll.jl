4c138365bef850a5477193dbefc43dbf85b5c17e
jlbuild <juliabuildbot@gmail.com>
2023-07-30 22:47:20 +0000

    Clipper2_jll build 1.2.2+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,19 +2,19 @@
 export libClipper2, libClipper2Z
 
 JLLWrappers.@generate_wrapper_header("Clipper2")
-JLLWrappers.@declare_library_product(libClipper2, "@rpath/libClipper2.dylib")
-JLLWrappers.@declare_library_product(libClipper2Z, "@rpath/libClipper2Z.dylib")
+JLLWrappers.@declare_library_product(libClipper2, "@rpath/libClipper2.1.dylib")
+JLLWrappers.@declare_library_product(libClipper2Z, "@rpath/libClipper2Z.1.dylib")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libClipper2,
-        "lib/libClipper2.dylib",
+        "lib/libClipper2.1.2.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libClipper2Z,
-        "lib/libClipper2Z.dylib",
+        "lib/libClipper2Z.1.2.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
